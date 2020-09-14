867037fdba746156d0e0e2da6d43365017496b13
MarcellDAvisse <71254724+MarcellDAvisse@users.noreply.github.com>
2020-09-14 10:13:27 +0200

    Add files via upload

---
+++ Default.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace WebbAppDB_Test
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnSelect_Click(object sender, EventArgs e)
+        {
+            string sqlstring = this.txtSelect.Text;
+            
+            string connStr = "server=localhost;database=webbapp;port=3306;user id=WebApp;password=root";
+
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try { 
+            
+                conn.Open();
+            }
+            catch (MySqlException)
+            {
+                //Datenbank nicht verfügbar
+                TableRow newRow = new TableRow();
+                TableCell newCell = new TableCell();
+                newCell.Text = "Datenbank nicht erreichbar";
+                newRow.Cells.Add(newCell);
+                tblSelect.Rows.Add(newRow);
+                return;
+            }
+
+
+            MySqlCommand command = new MySqlCommand(sqlstring, conn);
+
+            //Abfrage ausführen
+            MySqlDataReader rdr = command.ExecuteReader();
+
+            //Tabelle einrichten
+            TableHeaderRow Header = new TableHeaderRow();
+            TableHeaderCell Cell = new TableHeaderCell();
+            TableRow NewRow = new TableRow();
+            TableCell NewCell = new TableCell();
+            //Header der Tabelle
+            for (int spaltenindex = 0; spaltenindex < rdr.FieldCount; spaltenindex++)
+            {
+
+                Cell = new TableHeaderCell();
+                Cell.Text = rdr.GetName(spaltenindex);
+                Header.Cells.Add(Cell);
+            }
+            this.tblSelect.Rows.Add(Header);
+
+            //Datensätze in die Tabelle
+            while (rdr.Read())
+            {
+                NewRow = new TableRow();
+                for (int spaltenindex = 0; spaltenindex < rdr.FieldCount; spaltenindex++)
+                {
+                    NewCell = new TableCell();
+                    NewCell.Text = rdr.GetValue(spaltenindex).ToString();
+                    NewRow.Cells.Add(NewCell);
+                }
+                this.tblSelect.Rows.Add(NewRow);
+            }
+
+            //Reader schliessen
+            rdr.Close();
+            //Verbindung schliessen
+            conn.Close();
+        }
+    }
+}
---
+++ Default.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <automatisch generiert>
+//     Dieser Code wurde von einem Tool generiert.
+//
+//     Änderungen an dieser Datei können fehlerhaftes Verhalten verursachen und gehen verloren, wenn
+//     der Code neu generiert wird.
+// </automatisch generiert>
+//------------------------------------------------------------------------------
+
+namespace WebbAppDB_Test {
+    
+    
+    public partial class _Default {
+        
+        /// <summary>
+        /// btnSelect-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSelect;
+        
+        /// <summary>
+        /// txtSelect-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSelect;
+        
+        /// <summary>
+        /// lblErgebnisSelect-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblErgebnisSelect;
+        
+        /// <summary>
+        /// tblSelect-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table tblSelect;
+    }
+}
